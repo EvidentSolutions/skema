7b2b649fadc162634c86c6ce62ef7ee832f25dc7
Juha Komulainen <juha.komulainen@evident.fi>
2025-11-11 15:26:10 +0200

    Produce a default name for indices if no name is defined

--- src/main/kotlin/fi/evident/skema/output/NamingStrategy.kt
+++ src/main/kotlin/fi/evident/skema/output/NamingStrategy.kt
@@ -6,7 +6,7 @@ import fi.evident.skema.model.Table
 public interface NamingStrategy {
 
     public fun indexName(table: Table, index: Index): String =
-        index.name ?: ""
+        index.name ?: "idx_${table.name}_${index.columns.joinToString("_")}"
 
     public fun primaryKeyConstraintName(table: Table, columnNames: List<String>): String =
         "pk_${table.name}"
